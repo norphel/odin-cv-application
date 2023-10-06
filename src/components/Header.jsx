89242b6dbbd53ae6932b8673b0c449e16e5673c5
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-06 22:13:51 +0530

    initialize header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css'
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1>Hello</h1>
+            </header>
+        </>
+    )
+}
