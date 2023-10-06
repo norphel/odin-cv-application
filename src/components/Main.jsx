a0408ccd5d314d6532b822c1eaf0d5aca34b92bf
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-06 22:14:25 +0530

    initialize component for main content

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import '../styles/Main.css';
+
+export default function Main () {
+    return (
+        <>
+            <main>
+                <p>Update the main section!</p>
+            </main>
+        </>
+    )
+}
