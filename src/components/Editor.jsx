95c9d377f6a3b8fea6cdbfbd4a24d96158f65110
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-09 21:51:21 +0530

    add navigation for different sections of CV

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,34 @@
+import profile from '../assets/profile.svg';
+import work from '../assets/work.svg';
+import projects from '../assets/projects.svg'
+import education from '../assets/education.png';
+import skills from '../assets/skills.svg';
+
+function Navigation() {
+    return (
+        <nav>
+            <div className='active'>
+                <img src={profile} alt="profile icon" />
+            </div>
+            <div>
+                <img src={work} alt="work experience icon" />
+            </div>
+            <div>
+                <img src={projects} alt="projects icon" /></div>
+            <div>
+                <img src={education} alt="education icon" />
+            </div>
+            <div>
+                <img src={skills} alt="skills icon" />
+            </div>
+        </nav>
+    )
+}
+
 export default function Editor() {
     return (
-        <div className="editor"></div>
+        <div className="editor">
+            <Navigation />
+        </div>
     )
 }
