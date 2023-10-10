395519738f4889f2830e76ddb63396d377350763
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-10 22:18:16 +0530

    add static content for profile section of cv now

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,5 +1,64 @@
+import '../styles/Preview.css'
+import phoneIcon from '../assets/phone.svg';
+import emailIcon from '../assets/email.svg';
+import linkedIn from '../assets/linkedin.svg';
+import github from '../assets/github.svg';
+import personalWebsite from '../assets/personalWebsite.svg';
+
+function Contact({src, alt, contact}) {
+    return (
+        <div>
+            <img src={src} alt={alt} />
+            <p>{contact}</p>
+        </div>
+    )
+}
+
+function Link({src, alt, url}) {
+    return (
+        <a href={url}><img src={src} alt={alt} /></a>
+    )
+}
+
+function Profile() {
+    return (
+        <div className="profile">
+            <h1 className='name'>Tsering Norphel</h1>
+            <div className="contactsAndLinks">
+            <div className="contacts">
+                    <Contact src={phoneIcon} alt={'phone icon'} contact={'0000000000'} />
+                    <Contact src={emailIcon} alt={'email icon'} contact={'username@domain'} />
+                </div>
+                <div className="links">
+                    <Link src={linkedIn} alt={'linkedIn icon'} url={'https://linkedin.com/username'} />
+                    <Link src={github} alt={'github icon'} url={'https://github.com/username'} />
+                    <Link src={personalWebsite} alt={'personal website icon'} url={'https://username.domain'} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function WorkExperience() {
+
+}
+
+function Projects() {
+
+}
+
+function Education() {
+
+}
+
+function Skills() {
+
+}
+
 export default function Preview() {
     return (
-        <div className="preview"></div>
+        <div className="preview">
+            <Profile />
+        </div>
     )
 }
