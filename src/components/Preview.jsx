20529507c5eae8fefd5c8a8c655ac0e0b968007b
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-11 12:05:38 +0530

    move profile section into its own component

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,47 +1,7 @@
-import '../styles/Preview.css'
-import phoneIcon from '../assets/phone.svg';
-import emailIcon from '../assets/email.svg';
-import linkedIn from '../assets/linkedin.svg';
-import github from '../assets/github.svg';
-import personalWebsite from '../assets/personalWebsite.svg';
+import Profile from './Profile';
+import WorkExperience from './WorkExperience';
+import '../styles/Preview.css';
 
-function Contact({src, alt, contact}) {
-    return (
-        <div>
-            <img src={src} alt={alt} />
-            <p>{contact}</p>
-        </div>
-    )
-}
-
-function Link({src, alt, url}) {
-    return (
-        <a href={url}><img src={src} alt={alt} /></a>
-    )
-}
-
-function Profile() {
-    return (
-        <div className="profile">
-            <h1 className='name'>Tsering Norphel</h1>
-            <div className="contactsAndLinks">
-            <div className="contacts">
-                    <Contact src={phoneIcon} alt={'phone icon'} contact={'0000000000'} />
-                    <Contact src={emailIcon} alt={'email icon'} contact={'username@domain'} />
-                </div>
-                <div className="links">
-                    <Link src={linkedIn} alt={'linkedIn icon'} url={'https://linkedin.com/username'} />
-                    <Link src={github} alt={'github icon'} url={'https://github.com/username'} />
-                    <Link src={personalWebsite} alt={'personal website icon'} url={'https://username.domain'} />
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function WorkExperience() {
-
-}
 
 function Projects() {
 
@@ -59,6 +19,7 @@ export default function Preview() {
     return (
         <div className="preview">
             <Profile />
+            <WorkExperience />
         </div>
     )
 }
