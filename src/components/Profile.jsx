607147bbbfbbcd45c1e18595e210c6727c722f7f
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-11 12:03:45 +0530

    move profile section into its own component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,39 @@
+import phoneIcon from '../assets/phone.svg';
+import emailIcon from '../assets/email.svg';
+import linkedIn from '../assets/linkedin.svg';
+import github from '../assets/github.svg';
+import personalWebsite from '../assets/personalWebsite.svg';
+
+function Contact({src, alt, contact}) {
+    return (
+        <div>
+            <img src={src} alt={alt} />
+            <p>{contact}</p>
+        </div>
+    )
+}
+
+function Link({src, alt, url}) {
+    return (
+        <a href={url}><img src={src} alt={alt} /></a>
+    )
+}
+
+export default function Profile() {
+    return (
+        <div className="profile">
+            <h1 className='name'>FirstName LastName</h1>
+            <div className="contactsAndLinks">
+            <div className="contacts">
+                    <Contact src={phoneIcon} alt={'phone icon'} contact={'0000000000'} />
+                    <Contact src={emailIcon} alt={'email icon'} contact={'username@domain'} />
+                </div>
+                <div className="links">
+                    <Link src={linkedIn} alt={'linkedIn icon'} url={'https://linkedin.com/username'} />
+                    <Link src={github} alt={'github icon'} url={'https://github.com/username'} />
+                    <Link src={personalWebsite} alt={'personal website icon'} url={'https://username.domain'} />
+                </div>
+            </div>
+        </div>
+    )
+}
