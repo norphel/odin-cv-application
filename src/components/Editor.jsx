23c01fd57731b2f3f012ab8065a7e5e27d221012
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-12 11:50:00 +0530

    move nav to its own component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,29 +1,4 @@
-import profile from '../assets/profile.svg';
-import work from '../assets/work.svg';
-import projects from '../assets/projects.svg'
-import education from '../assets/education.svg';
-import skills from '../assets/skills.svg';
-
-function Navigation() {
-    return (
-        <nav>
-            <div className='active'>
-                <img src={profile} alt="profile icon" />
-            </div>
-            <div>
-                <img src={work} alt="work experience icon" />
-            </div>
-            <div>
-                <img src={projects} alt="projects icon" /></div>
-            <div>
-                <img src={education} alt="education icon" />
-            </div>
-            <div>
-                <img src={skills} alt="skills icon" />
-            </div>
-        </nav>
-    )
-}
+import Navigation from "./Navigation"
 
 export default function Editor() {
     return (
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import profile from '../assets/profile.svg';
+import work from '../assets/work.svg';
+import projects from '../assets/projects.svg'
+import education from '../assets/education.svg';
+import skills from '../assets/skills.svg';
+
+export default function Navigation() {
+    return (
+        <nav>
+            <div className='active'>
+                <img src={profile} alt="profile icon" />
+            </div>
+            <div>
+                <img src={work} alt="work experience icon" />
+            </div>
+            <div>
+                <img src={projects} alt="projects icon" /></div>
+            <div>
+                <img src={education} alt="education icon" />
+            </div>
+            <div>
+                <img src={skills} alt="skills icon" />
+            </div>
+        </nav>
+    )
+}
