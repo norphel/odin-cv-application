be320ef3ca069901d05398be82b9503f76148720
Norphel <113838216+norphel@users.noreply.github.com>
2023-10-13 14:42:04 +0530

    add work experience editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./Navigation"
 import ProfileEditor from "./ProfileEditor"
+import WorkExperienceEditor from "./WorkExperienceEditor"
 
 export default function Editor() {
     return (
         <div className="editor">
             <Navigation />
             <ProfileEditor />
+            <WorkExperienceEditor />
         </div>
     )
 }
---
+++ src/components/WorkExperienceEditor.jsx
@@ -0,0 +1,31 @@
+function WorkDetails() {
+    return (
+        <>
+            <label htmlFor="position" id="position">Position
+                <input id="position" htmlFor="position" type="text" />
+            </label>
+            <label htmlFor="organization" id="organization">Organization
+                <input id="organization" htmlFor="organization" type="text" />
+            </label>
+            <label htmlFor="fromDate" id="fromDate">From
+                <input id="fromDate" htmlFor="fromDate" type="date" />
+            </label>
+            <label htmlFor="toDate" id="toDate">Till
+                <input id="toDate" htmlFor="toDate" type="date" />
+            </label>
+            <label htmlFor="description" id="description">Contribution/Impact
+                <textarea id="description" htmlFor="description" rows={'5'}/>
+            </label>
+            <button type="button">Add More</button>
+        </>
+    )
+}
+
+export default function WorkExperienceEditor() {
+    return (
+        <div className="workExperienceEditor active">
+            <h2>Work Experience</h2>
+            <WorkDetails />
+        </div>
+    )
+}
