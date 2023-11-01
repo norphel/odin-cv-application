b6e68cd55d2867a2317131a2a9d82cf3ee3e228b
Norphel <113838216+norphel@users.noreply.github.com>
2023-11-01 22:28:38 +0530

    add style to render newline in text area input to preview

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ function Project({title, description}) {
     return (
         <div>
             <h3>{title}</h3>
-            <p>{description}</p>
+            <p className="description">{description}</p>
         </div>
     )
 }
